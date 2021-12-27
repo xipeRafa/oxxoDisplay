1cad6501a9d7668142c2a28a1c320f96805f48f1
rafael <rafaelsistem87@gmail.com>
2021-12-27 07:26:13 -0700

    distrito dos

--- src/components/auctions/AuctionBody.js
+++ src/components/auctions/AuctionBody.js
@@ -315,7 +315,7 @@ console.log('n:', n)
               {/* <span className="bg-danger p-1">
                 {n?.filter((el) => el.completed === false).length}
               </span> */}{" "}
-              Viajes Para {mail}
+              Viajes Para Tienda: {mail}
             </span>
           </div>
           <div className="col-1"></div>
@@ -457,13 +457,226 @@ console.log('n:', n)
 
           </div>
 
+          <div className="col-md-3 text-center">
+            <select className={arr.length> 0 ? "w-100 form-control mt-2" : "d-none"} 
+                    onChange={handleMail} >
 
+                 <option>Selecciona Una Tienda de Distrito 2</option>
 
+                <option value="14 De Abril-503FS">14 De Abril-503FS</option> 
+                <option value="20 de Noviembre-50NOM">20 de Noviembre-50NOM</option> 
+                <option value="Abastos-50ABK">Abastos-50ABK</option> 
 
+                <option value="Adelina-5075V">Adelina-5075V</option> 
+                <option value="Agualurca-50AWL">Agualurca-50AWL</option> 
+                <option value="Alborada-50GVZ">Alborada-50GVZ</option> 
 
+                <option value="Alegranza-50X6D">Alegranza-50X6D</option> 
+                <option value="Bacadehuachi-50V0O">Bacadehuachi-50V0O</option> 
+                <option value="Baviacora-50LYQ">Baviacora-50LYQ</option> 
 
+                <option value="Belice-50BEJ">Belice-50BEJ</option> 
+                <option value="Boulevard-50OBU">Boulevard-50OBU</option> 
+                <option value="California-505QJ">California-505QJ</option> 
 
+                <option value="Campestre-50VJT">Campestre-50VJT</option> 
+                <option value="Casa Bonita-506I7">Casa Bonita-506I7</option> 
+                <option value="Catedral-50JNC">Catedral-50JNC</option> 
 
+                <option value="caturegli-50J25">caturegli-50J25</option> 
+                <option value="Centenario-507HN">Centenario-507HN</option> 
+                <option value="Central-50QQF">Central-50QQF</option> 
+
+                <option value="Centro-50CVU">Centro-50CVU</option> 
+                <option value="Chavez-50GIY">Chavez-50GIY</option> 
+                <option value="Cien-50CNI">Cien-50CNI</option> 
+
+                <option value="Cinco de Mayo-50HSM">Cinco de Mayo-50HSM</option> 
+                <option value="Clinica 14-50DLC">Clinica 14-50DLC</option> 
+                <option value="ClInica 37-50E5D">ClInica 37-50E5D</option> 
+
+                <option value="Clinica 68-500N5">Clinica 68-500N5</option> 
+                <option value="Coahuila-50HUC">Coahuila-50HUC</option> 
+                <option value="Colosio-50YIP">Colosio-50YIP</option> 
+
+                <option value="Condesa-50HOC">Condesa-50HOC</option> 
+                <option value="Conquistadores-50COQ">Conquistadores-50COQ</option> 
+                <option value="Constitucion-50DOM">Constitucion-50DOM</option> 
+
+                <option value="Coras-50ZYF">Coras-50ZYF</option> 
+                <option value="Country Club-50RPM">Country Club-50RPM</option> 
+                <option value="Crit-50FYC">Crit-50FYC</option> 
+
+                <option value="Cuauhtemoc-50CCE">Cuauhtemoc-50CCE</option> 
+                <option value="De la Nacion-50DLB">De la Nacion-50DLB</option> 
+                <option value="Del Prado-50HBB">Del Prado-50HBB</option> 
+
+                <option value="Diez-50W9V">Diez-50W9V</option> 
+                <option value="Doctor Noriega-50V1T">Doctor Noriega-50V1T</option> 
+                <option value="Donato Guerra-50AIX">Donato Guerra-50AIX</option> 
+
+                <option value="Ebanos-50UFA">Ebanos-50UFA</option> 
+                <option value="El Puente-50LYZ">El Puente-50LYZ</option> 
+                <option value="El Seri-50HQR">El Seri-50HQR</option> 
+
+                <option value="El Triunfo-50EU4">El Triunfo-50EU4</option> 
+                <option value="Estacion 26-50ESV">Estacion 26-50ESV</option> 
+                <option value="Fresnos-502JH">Fresnos-502JH</option> 
+
+                <option value="Gala-501P9">Gala-501P9</option> 
+                <option value="Ganadera-50GDR">Ganadera-50GDR</option> 
+                <option value="Garcia Morales-50004">Garcia Morales-50004</option> 
+
+                <option value="Garcia-50YIW">Garcia-50YIW</option> 
+                <option value="Garmendia-50QWQ">Garmendia-50QWQ</option> 
+                <option value="Gomez Farias-50DDM">Gomez Farias-50DDM</option> 
+
+                <option value="Guamuchil-508EN">Guamuchil-508EN</option> 
+                <option value="Guerrero-50MMU">Guerrero-50MMU</option> 
+                <option value="Haciendas del Sur-50IHP">Haciendas del Sur-50IHP</option> 
+
+                <option value="Healy-50IVJ">Healy-50IVJ</option> 
+                <option value="Hermosillo-50BIB">Hermosillo-50BIB</option> 
+                <option value="Industrial-Industrial">Industrial-Industrial</option> 
+
+                <option value="Infonavit-50FON">Infonavit-50FON</option> 
+                <option value="Internacional-50ITE">Internacional-50ITE</option> 
+                <option value="Israel-50LSR">Israel-50LSR</option> 
+
+                <option value="jefatura-50p52">jefatura-50p52</option> 
+                <option value="Jose Carmelo-50LXB">Jose Carmelo-50LXB</option> 
+                <option value="Juarez-50UMU">Juarez-50UMU</option> 
+
+                <option value="Justo Sierra-50ACK">Justo Sierra-50ACK</option> 
+                <option value="La Caridad-50XNU">La Caridad-50XNU</option> 
+                <option value="La Morrita-50EIW">La Morrita-50EIW</option> 
+
+                <option value="Las Palmas-50HPM">Las Palmas-50HPM</option> 
+                <option value="Las Quintas-50XJB">Las Quintas-50XJB</option> 
+                <option value="Lauro Galvez-50YVR">Lauro Galvez-50YVR</option> 
+
+                <option value="Lazaro Cardenas-50JWV">Lazaro Cardenas-50JWV</option> 
+                <option value="Lazaro Mercado-50FLM">Lazaro Mercado-50FLM</option> 
+                <option value="Loma Linda-50LOL">Loma Linda-50LOL</option> 
+
+                <option value="Lomas del Sur-50HSL">Lomas del Sur-50HSL</option> 
+                <option value="Lomas Pitic-50LPT">Lomas Pitic-50LPT</option> 
+                <option value="Lopez Mateos-50SOD">Lopez Mateos-50SOD</option> 
+
+                <option value="Los Arcos-50ARC">Los Arcos-50ARC</option> 
+                <option value="Madero-50INY">Madero-50INY</option> 
+                <option value="Madrid-50XND">Madrid-50XND</option> 
+
+                <option value="Mallorca-50XJC">Mallorca-50XJC</option> 
+                <option value="Manglar-50JAB">Manglar-50JAB</option> 
+                <option value="Manzanares-50NZN">Manzanares-50NZN</option> 
+
+                <option value="Mariachi-50HEM">Mariachi-50HEM</option> 
+                <option value="Marquez-50MHG">Marquez-50MHG</option> 
+                <option value="Marsella-50KMZ">Marsella-50KMZ</option> 
+
+                <option value="Matamoros-50MWT">Matamoros-50MWT</option> 
+                <option value="Mercedes-50HGC">Mercedes-50HGC</option> 
+                <option value="Milenium-50AZW">Milenium-50AZW</option> 
+
+                <option value="Mocorito-50XKI">Mocorito-50XKI</option> 
+                <option value="Modelo-50MOD">Modelo-50MOD</option> 
+                <option value="Monaco-500I8">Monaco-500I8</option> 
+
+                <option value="Monterrey-50NWY">Monterrey-50NWY</option> 
+                <option value="Monteverde-50OMV">Monteverde-50OMV</option> 
+                <option value="Morelia-50LIA">Morelia-50LIA</option> 
+
+                <option value="Nacori Grande-50JQY">Nacori Grande-50JQY</option> 
+                <option value="Naranjos-50NJO">Naranjos-50NJO</option> 
+                <option value="Navarrete-50NRR">Navarrete-50NRR</option> 
+
+                <option value="Navojoa-50FJU">Navojoa-50FJU</option> 
+                <option value="Nayarit-50NYT">Nayarit-50NYT</option> 
+                <option value="Noroeste-50NRO">Noroeste-50NRO</option> 
+
+                <option value="Nueva Esperanza-507ZG">Nueva Esperanza-507ZG</option> 
+                <option value="Palacios-50JRJ">Palacios-50JRJ</option> 
+                <option value="Palma Dorada-503R3">Palma Dorada-503R3</option> 
+
+                <option value="Periferico-50HPR">Periferico-50HPR</option> 
+                <option value="Perimetral-50PRL">Perimetral-50PRL</option> 
+                <option value="Periodista-509S1">Periodista-509S1</option> 
+
+                <option value="Perisur-50RPS">Perisur-50RPS</option> 
+                <option value="Pina-50HPI">Pina-50HPI</option> 
+                <option value="Pirul-504AU">Pirul-504AU</option> 
+
+                <option value="Pitahaya Madura-50WL2">Pitahaya Madura-50WL2</option> 
+                <option value="Pitic-50OPT">Pitic-50OPT</option> 
+                <option value="Planetario-50DNQ">Planetario-50DNQ</option> 
+
+                <option value="Pueblo Nuevo">Pueblo Nuevo</option> 
+                <option value="Puerta del rey-50LRO">Puerta del rey-50LRO</option> 
+                <option value="Puerta Real-50PUT">Puerta Real-50PUT</option> 
+
+                <option value="Quinta Emilia-50IVQ">Quinta Emilia-50IVQ</option> 
+                <option value="Quintas del Sol-50CYX">Quintas del Sol-50CYX</option> 
+                <option value="Quiroga-50IDF">Quiroga-50IDF</option> 
+
+                <option value="Real de Minas-50MDM">Real de Minas-50MDM</option> 
+                <option value="Real-50RJP">Real-50RJP</option> 
+                <option value="Rebeico-50LT1">Rebeico-50LT1</option> 
+
+                <option value="Reforma-50REF">Reforma-50REF</option> 
+                <option value="Revolucion-50VOL">Revolucion-50VOL</option> 
+                <option value="Romero-50JRB">Romero-50JRB</option> 
+
+                <option value="Ruba-50RBI">Ruba-50RBI</option> 
+                <option value="Salazar-50AUD">Salazar-50AUD</option> 
+                <option value="San Agustin-50YAB">San Agustin-50YAB</option> 
+
+                <option value="San Angel-50SAA">San Angel-50SAA</option> 
+                <option value="San Benito-50HSB">San Benito-50HSB</option> 
+                <option value="san bernardino-50X9V">san bernardino-50X9V</option> 
+
+                <option value="Seguro Social-50OXS">Seguro Social-50OXS</option> 
+                <option value="Serna-50SEA">Serna-50SEA</option> 
+                <option value="Solidaridad-50FSL">Solidaridad-50FSL</option> 
+
+                <option value="Sonacer-50SQJ">Sonacer-50SQJ</option> 
+                <option value="Sonora-50FYU">Sonora-50FYU</option> 
+                <option value="Super Salcido-50FFV">Super Salcido-50FFV</option> 
+
+                <option value="Super Tierra Nueva II-50BHR">
+                  Super Tierra Nueva II-50BHR
+                </option> 
+                <option value="Tepoca-50NCG">Tepoca-50NCG</option> 
+                <option value="Topahue-50TPU">Topahue-50TPU</option> 
+
+                <option value="Transversal-50HTV">Transversal-50HTV</option> 
+                <option value="Transversal-Monteverde-50OMV">
+                  Transversal-Monteverde-50OMV
+                </option> 
+                <option value="Triana-50WSY">Triana-50WSY</option> 
+
+                <option value="Universidad II-50USI">Universidad II-50USI</option> 
+                <option value="Valentina-50AUM">Valentina-50AUM</option> 
+                <option value="Valle de Lago-50VGK">Valle de Lago-50VGK</option> 
+
+                <option value="Verbena-50AJV">Verbena-50AJV</option> 
+                <option value="Vergel-50BYP">Vergel-50BYP</option> 
+                <option value="Vertedor-50VDK">Vertedor-50VDK</option> 
+
+                <option value="Villa Bonita-50KVB">Villa Bonita-50KVB</option> 
+                <option value="Villa Dorada-50VDB">Villa Dorada-50VDB</option> 
+                <option value="Villa Hermosa-50FXI">Villa Hermosa-50FXI</option> 
+
+                <option value="Villa Quintero-50R5C">Villa Quintero-50R5C</option> 
+                <option value="Villas del Sur-50VIW">Villas del Sur-50VIW</option> 
+                <option value="Volcanes-50UTB">Volcanes-50UTB</option> 
+
+                <option value="Y Griega-50GRI">Y Griega-50GRI</option> 
+                <option value="Yanez-50BYY">Yanez-50BYY</option> 
+
+            </select>
+
+          </div>
 
           {/* <div className="col-1"></div>
            <div className={n?.length > 0 ? "col-md-3 mt-2 " : "d-none"}>
